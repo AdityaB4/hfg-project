aff6721171aabc1c48951d3e12a9bcb2e28bd42f
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-02-09 21:33:35 +0800

    Add firebase config

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("./hfg-project-fe558-firebase-adminsdk-6m0wo-b22b5aa40a.json")
+firebase_admin.initialize_app(cred)
+
+
 #insert valid token
 Token = ""
 updater = Updater(Token, use_context = True)
